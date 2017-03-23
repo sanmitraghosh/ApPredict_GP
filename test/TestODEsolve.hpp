4de50bafeac79948dfba3f84f998ee63e40251ba
Sanmitra Ghosh <sanmitra25@hotmail.com>
2017-03-23 11:15:57 +0000

    remove tut

--- test/TestODEsolve.hpp
+++ test/TestODEsolve.hpp
@@ -5,16 +5,17 @@
 #include "CellProperties.hpp"
 #include "SteadyStateRunner.hpp"
 #include "AbstractCvodeCell.hpp"
+#include "AbstractCardiacCell.hpp"
 #include "RegularStimulus.hpp"
 #include "EulerIvpOdeSolver.hpp"
 #include "Shannon2004Cvode.hpp"
 #include "ohara_rudy_2011_endo.hpp"
 #include "ohara_rudy_2011_endoOpt.hpp"
-#include "ohara_rudy_2011_endoCvode.hpp"
-#include "ohara_rudy_2011_endoCvodeOpt.hpp"
+#include "ohara_rudy_2011_epiCvode.hpp"
+
 #include "FakePetscSetup.hpp"
 #include <iostream>
-
+#include <fstream>
 class TestSingleCellTutorial : public CxxTest::TestSuite
 {
 public:
@@ -23,20 +24,31 @@ public:
 #ifdef CHASTE_CVODE
         boost::shared_ptr<RegularStimulus> p_stimulus;
         boost::shared_ptr<AbstractIvpOdeSolver> p_solver;
-        boost::shared_ptr<AbstractCvodeCell> p_model(new Cellohara_rudy_2011_endoFromCellMLCvode(p_solver, p_stimulus));
+        boost::shared_ptr<AbstractCvodeCell> p_model(new CellShannon2004FromCellMLCvode(p_solver, p_stimulus));
 
         boost::shared_ptr<RegularStimulus> p_regular_stim = p_model->UseCellMLDefaultStimulus();
 
+/*       AbstractCardiacCell* p_chaste_cell;
+        p_chaste_cell = new CellShannon2004FromCellML(p_solver, p_stimulus);*/
+
+
         p_regular_stim->SetPeriod(1000.0);
-        std::cout << "Param is.\n"<<p_model->GetParameter("membrane_persistent_sodium_current_conductance")<<"\n";
-        double kparam = p_model->GetParameter("membrane_persistent_sodium_current_conductance");
-        p_model->SetParameter("membrane_persistent_sodium_current_conductance", 0.1*kparam);
+        std::cout << p_model->GetSystemName() << std::endl << std::flush;
+        std::cout << "Param is.\n"<<p_model->GetNumberOfParameters()<<std::endl<< std::flush;
+        const std::vector<std::string>par= p_model->rGetParameterNames();
+        for(unsigned i = 0; i != par.size(); i++) {
+           std::cout << par[i]<<std::endl<< std::flush;
+        }
+       //std::cout <<par[0]<<std::endl<< std::flush;
+        //double kparam = p_model->GetParameter("membrane_persistent_sodium_current_conductance");
+        //p_model->SetParameter("membrane_persistent_sodium_current_conductance", 0.1*kparam);
         SteadyStateRunner steady_runner(p_model);
-        steady_runner.SetMaxNumPaces(100u);
+        steady_runner.SetMaxNumPaces(1000u);
         bool result;
         result = steady_runner.RunToSteadyState();
 
         TS_ASSERT_EQUALS(result,false);
+
 
         double max_timestep = 0.1;
         p_model->SetMaxTimestep(max_timestep);
@@ -56,6 +68,14 @@ public:
         std::cout << "APD value is.\n"<<apd;
 //        TS_ASSERT_DELTA(apd, 212.41, 1e-2);
 //      TS_ASSERT_DELTA(upstroke_velocity, 338, 1.25);
+             std::ofstream myfile;
+              myfile.open ("example.csv");
+              myfile << "This is the first cell in the first column.\n";
+              myfile << "a,b,c,\n";
+              myfile << "c,s,v,\n";
+              myfile << "1,2,3.456\n";
+              myfile << "semi;colon";
+              myfile.close();
 
 #else
         std::cout << "Cvode is not enabled.\n";
