0b1e7cd7f953f66af9645ed42af7ea53dddfeeb9
Sanmitra Ghosh <sanmitra25@hotmail.com>
2018-03-09 16:50:05 +0000

    "boundaryN"

--- MATLAB/GPpredict/boundaryDetector.m
+++ MATLAB/GPpredict/boundaryDetector.m
@@ -1,6 +1,5 @@
 function [y_star_class, R_star_AP, y_star_AP] = boundaryDetector(R, y, R_star, HyperParams, y_true)
  
-disp('Using Correct multidomain')
 [ R_train,y_train ] = labelFinder( R, y);
 HyperParams.hyp=HyperParams.hyp1;
 [s1, lpNR ] = classifierGP( R_train, y_train(:,1), R_star, HyperParams);
@@ -9,24 +8,8 @@ HyperParams.hyp=HyperParams.hyp2;
 HyperParams.hyp=HyperParams.hyp3;
 [s3, lpND ] = classifierGP( R_train, y_train(:,3), R_star, HyperParams);
 
-lp1=lpNR;lp2=lpD;lp3=lpND;
-    
-% for i=1:length(R_star)
-%  [a,b]=max([exp(lp1(i)),exp(lp2(i)),exp(lp3(i))]);
-% %      [c,d]=max([s1(i),s2(i),s3(i)]);
-%     if b==1 
-%         certainty(i)=exp(lp1(i))-max([exp(lp2(i)),exp(lp3(i))]); 
-%     elseif b==2  
-%         certainty(i)=exp(lp2(i))-max([exp(lp1(i)),exp(lp3(i))]);
-%     elseif b==3 
-%         certainty(i)=exp(lp3(i))-max([exp(lp1(i)),exp(lp2(i))]);
-%     end
-% 
-% end
-% certainty=certainty';
 
-
-%%%%%% Find seperate domains
+%%%%%% Perform OVR classification
 j=1;
 k=1;
 l=1;
